6ff2ceedc576a044bcc7fefe7c93a0c8202a8405
Zakaria <zakariamesrar49@gmail.com>
2024-05-12 20:39:56 +0100

    final  commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Auth from './pages/Auth/Auth';
 
+function RedirectToMain() {
+  window.location.href = "/Adminpage/cars";
+  return null;
+}
+
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route exact path="/" element={<RedirectToMain />} />
         <Route path="/Adminpage/*" element={<Dashboard />} />
         <Route path="/Auth" element={<Auth />} />
       </Routes>
